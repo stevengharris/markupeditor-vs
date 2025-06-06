541451e0de54e214b6395c302f7de0b0aa8b3d1d
Steven G. Harris <steven.g.harris@gmail.com>
2025-06-06 14:40:55 -0700

    Use symlinks to markupeditor dist and styles

---
+++ precompile.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# To ensure the VSCode extension uses the current markupeditor script and styles, 
+# place symlinks in the scripts and styles directory for the extension. This script
+# is executed as a precompile step.
+pushd ./scripts
+ln -sf ../node_modules/markupeditor/dist/markupeditor.umd.js markupeditor.js
+popd
+pushd ./styles
+ln -sf ../node_modules/markupeditor/styles/mirror.css mirror.css
+ln -sf ../node_modules/markupeditor/styles/markup.css markup.css
+ln -sf ../node_modules/markupeditor/styles/toolbar.css toolbar.css
+popd
